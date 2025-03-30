b741396c73edd417039bef5a016469c2f1c38141
mhesemans <micha_hesemans@hotmail.com>
2025-03-30 14:36:41 +0100

    feat: added urls for news posts

--- docdirect/urls.py
+++ docdirect/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from news.views import home_news
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_news, name='news'),
+    path('', include('news.urls'), name='news-urls'),
 
 ]
---
+++ news/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.NewsList.as_view(), name='home'),
+    path('<slug:slug>/', views.news_detail, name='news_detail'),
+]
