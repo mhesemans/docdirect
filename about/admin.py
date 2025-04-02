6164452c4bdce9caea9902398deb868e3bf9279c
mhesemans <micha_hesemans@hotmail.com>
2025-04-02 08:03:41 +0100

    feat: registered about model in admin and migrated

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import StaffMember
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(StaffMember)
+class StaffMemberAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'role', 'status')
+    list_filter = ('role', 'status')
+    search_fields = ('name', 'role')
+    summernote_fields = ('content',)
