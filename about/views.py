a861165fdd9629283af5f8336d69b7af0e757e8e
mhesemans <micha_hesemans@hotmail.com>
2025-04-03 22:51:12 +0100

    feat: added about template

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import StaffMember
 
-# Create your views here.
+
+def about_view(request):
+    staff = StaffMember.objects.filter(status=1).order_by('name')  # only published
+    return render(request, "about/about.html", {"staff": staff})
