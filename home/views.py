6b8b9a8ea3905e7b3b41760d97e102d6103b829c
mhesemans <micha_hesemans@hotmail.com>
2025-04-03 22:50:41 +0100

    feat: added home app with template and urls

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from news.models import News
 
 def home_view(request):
-    return render(request, "home/home.html")
+    latest_news = News.objects.filter(status=1).order_by('-created_on').first()
+    return render(request, "home/home.html", {
+        'latest_news': latest_news
+    })
