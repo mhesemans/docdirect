250b81c38e1164231ce5fd0170129be14ae8c7a3
mhesemans <micha_hesemans@hotmail.com>
2025-04-03 23:04:08 +0100

    feat: cleaned up about.html template, corrected url

--- docdirect/urls.py
+++ docdirect/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = 
-[
-    path('about/', include('about.urls'))
+urlpatterns = [ 
+    path('about/', include('about.urls')),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # Home as root URL
     path('summernote/', include('django_summernote.urls')),
