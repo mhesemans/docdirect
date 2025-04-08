27e4039906c2c527fec9f048422d387405ebd2cf
mhesemans <micha_hesemans@hotmail.com>
2025-04-08 19:34:33 +0100

    feat: collectstatic and set up for heroku deployment

--- docdirect/settings.py
+++ docdirect/settings.py
@@ -80,6 +80,7 @@ ACCOUNT_FORMS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
